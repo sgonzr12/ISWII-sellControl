7ba138bd466712a6df887d0d8bd63a87e6346cb8
RubenFdezGlez <rferng19@estudiantes.unileon.es>
2025-04-24 19:24:13 +0200

    feat: implement login and authorization endpoints

--- backend/Landing.py
+++ backend/Landing.py
@@ -1,11 +1,13 @@
 # Import necessary libraries
-from fastapi import FastAPI, HTTPException, Cookie
+from fastapi import FastAPI, HTTPException, Cookie, Request,
 from authlib.integrations.starlette_client import OAuth
 from starlette.middleware.sessions import SessionMiddleware
 from dotenv import load_dotenv
 import os
 from datetime import datetime, timedelta, timezone
 from jose import jwt, ExpiredSignatureError, JWTError
+from fastapi.responses import RedirectResponse
+
 
 # Start FastAPI app and configure session middleware
 app = FastAPI()
@@ -54,3 +56,56 @@ def get_current_user(token: str = Cookie(None)):
         raise HTTPException(status_code = 401, detail = "Token expired")
     except JWTError:
         raise HTTPException(status_code = 401, detail = "Invalid token")
+    
+@router.get("/login")
+async def login(request: Request):
+    request.session.clear()
+    frontend_url = os.getenv("FRONTEND_URL")
+    redirect_url = os.getenv("REDIRECT_URL")
+    request.session["login_redirect"] = frontend_url 
+
+    return await oauth.auth_demo.authorize_redirect(request, redirect_url, prompt = "consent")
+
+@router.route("/auth")
+async def auth(request: Request):
+    try:
+        # Exchange the authorization code from Google consent for an access token
+        token = await oauth.auth_demo.authorize_access_token(request)
+    except Exception as e:
+        raise HTTPException(status_code = 401, detail = "Google authentication failed.")
+
+    try:
+        user_info_endpoint = "https://www.googleapis.com/oauth2/v2/userinfo" # Endpoint to get user information
+        headers = {"Authorization": f'Bearer {token["access_token"]}'} # Uses the token to access this
+        google_response = requests.get(user_info_endpoint, headers=headers)
+    except Exception as e:
+        raise HTTPException(status_code = 401, detail = "Google authentication failed.")
+
+    user = token.get("userinfo")
+    expires_in = token.get("expires_in")
+    user_id = user.get("sub")
+    iss = user.get("iss") # Issuer (Google)
+    user_email = user.get("email")
+
+    # Check to see if the token was issued from Google (Prevent token forgery)
+    if iss not in ["https://accounts.google.com", "accounts.google.com"]:
+        raise HTTPException(status_code = 401, detail = "Google authentication failed.")
+
+    if user_id is None:
+        raise HTTPException(status_code = 401, detail = "Google authentication failed.")
+
+    # Create JWT token
+    access_token_expires = timedelta(seconds = expires_in)
+    access_token = create_access_token(data = {"sub": user_id, "email": user_email}, expires_delta = access_token_expires)
+
+    redirect_url = request.session.pop("login_redirect", "")
+    response = RedirectResponse(redirect_url) # Redirect to the inicial page after login
+    response.set_cookie(
+        key = "access_token",
+        value = access_token,
+        httponly = True, # Prevents JS access to the cookie
+        secure = True,  # Ensure Cookie is sent over HTTPS
+        samesite = "strict",  # Sends the cookie only to the same site
+    )
+
+    return response
