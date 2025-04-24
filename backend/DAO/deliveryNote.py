0ca3fca815bac36989f6fcb6c46a772803be12c1
sergio <sgonzr12@estudiantes.unileon.es>
2025-04-24 18:54:34 +0200

    feat: clases de items

--- backend/DAO/deliveryNote.py
+++ backend/DAO/deliveryNote.py
@@ -1,19 +1,19 @@
 from datetime import date  
-from product import Product  
+from product import Product
 from typing import Optional, List, Tuple  
 
-class Ofert:
-    def __init__(self, ofertID: int, employeId: int, clientId: int, ofert_date: Optional[date] = None, 
+class deliveryNote:
+    def __init__(self, deliveryNoteID: int, employeId: int, clientId: int, deliveryNoteDate: Optional[date] = None, 
                  totalPrize: Optional[float] = None, products: Optional[List[Tuple[Product, int]]] = None):
-        self.ofertID = ofertID
+        self.deliveryNoteID = deliveryNoteID
         self.employeId = employeId
         self.clientId = clientId
-        self.date = ofert_date if ofert_date is not None else date.today()
+        self.date = deliveryNoteDate if deliveryNoteDate is not None else date.today()
         self.products = products if products is not None else []
         self.totalPrize = totalPrize if totalPrize is not None else self.calculatePrice(self.products)
 
     def __repr__(self):
-        return (f"Ofert(ofertID={self.ofertID}, employeId={self.employeId}, "
+        return (f"deliveryNote(deliveryNoteID={self.deliveryNoteID}, employeId={self.employeId}, "
                 f"clientId={self.clientId}, date={self.date}, totalPrize={self.totalPrize}, "
                 f"products={self.products})")
     
