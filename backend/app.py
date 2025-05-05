afaaea264c37ddc5a6f99a43e1e7519edb26cdea
sergio <sgonzr12@estudiantes.unileon.es>
2025-05-05 19:59:13 +0200

    fix: token verification

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,17 @@
 from fastapi import Depends, HTTPException
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials 
 from dotenv import load_dotenv
+from google.oauth2 import id_token
+from google.auth.transport import requests as google_requests
 import jwt
 import os
-import requests
 
 security = HTTPBearer()
     
 # Load environment variables from .env file
-load_dotenv("../ps.env")
+env_path = os.path.join(os.path.dirname(__file__), "../ps.env")
+
+load_dotenv(env_path)
 CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
 CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
 
@@ -26,21 +29,14 @@ def verifyToken(token: HTTPAuthorizationCredentials = Depends(security)) -> None
     if CLIENT_ID is None or CLIENT_SECRET is None:
         raise HTTPException(status_code=500, detail="Missing environment variables")
 
-
     # Verify the token
     try:
-        # Check the token with the Google API
-        url = "https://oauth2.googleapis.com/tokeninfo?id_token=" + credentials
-        response = requests.get(url)
-        if response.status_code != 200:
-            raise HTTPException(status_code=401, detail="Token inválido o expirado")
-        user_info = response.json()
-        if user_info.get("aud") != CLIENT_ID:
-            raise HTTPException(status_code=401, detail="Token emitido para otra aplicacion")
-        
-        # Decode the token using the client ID and secret
-        jwt.decode(credentials, CLIENT_SECRET, algorithms=["HS256"], audience=CLIENT_ID)
-        
+        id_token.verify_oauth2_token(  # type: ignore
+            credentials,
+            request=google_requests.Request(),
+            audience=CLIENT_ID
+        )
+    
     except jwt.ExpiredSignatureError:
         raise HTTPException(status_code=401, detail="Token has expired")
     except jwt.InvalidTokenError:
