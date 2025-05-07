b6974492af227912b30a87ebf279f51cabd7f761
sgonzr12 <sgonzr12@estudiantes.unileon.es>
2025-05-07 19:06:58 +0200

    feat: change cross origin policy

--- backend/main.py
+++ backend/main.py
@@ -10,6 +10,7 @@ import user
 env_path = os.path.join(os.path.dirname(__file__), "../ps.env")
 load_dotenv(env_path)
 PORT = os.getenv("PORT")
+FRONTEND_URL = os.getenv("FRONTEND_URL")
 
 
 #TODO: change prints with logger (branch database)
@@ -21,10 +22,13 @@ if __name__ == "__main__":
     #get app instance
     app = FastAPI()
     
+    if FRONTEND_URL is None:
+        raise ValueError("FRONTEND_URL environment variable not set")
+    
     # TODO: change to allow only specific origins in production
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["*"],  # Allow all origins
+        allow_origins=[FRONTEND_URL],  # Allow all origins
         allow_credentials=True,
         allow_methods=["*"],  # Allow all HTTP methods
         allow_headers=["*"],  # Allow all headers
